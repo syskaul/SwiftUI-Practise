ddf3e89de0a10f892dade22340633512ef705fcf
Eshaan Kaul <eshaankaul29@gmail.com>
2020-09-13 00:17:05 +0530

    Map view added

--- SwiftUI Practise/SwiftUI Practise/MapView.swift
+++ SwiftUI Practise/SwiftUI Practise/MapView.swift
@@ -9,16 +9,25 @@
 import SwiftUI
 import MapKit
 
-struct MapView: View {
-    var body: some View {
-         
+struct MapView: UIViewRepresentable {
+   
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+           MKMapView()
+           
+       }
+       
+       
+       
+       func updateUIView(_ uiView: MapView.UIViewType, context: UIViewRepresentableContext<MapView>) {
+           
         
-        
-        
-        
-        
-        
-    }
+            let coordinate = CLLocationCoordinate2D(
+               latitude: 28.462353, longitude: 77.030712)
+           let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.01)
+           let region = MKCoordinateRegion(center: coordinate, span: span)
+           uiView.setRegion(region, animated: true)
+       }
+    
 }
 
 struct MapView_Previews: PreviewProvider {

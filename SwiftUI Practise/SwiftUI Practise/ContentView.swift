6ff96f66b10deee356fa1d2536d167d1b5348db7
Eshaan Kaul <eshaankaul29@gmail.com>
2020-09-12 21:43:11 +0530

    Starting off SwiftUI

---
+++ SwiftUI Practise/SwiftUI Practise/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI Practise
+//
+//  Created by Eshaan Kaul on 12/09/20.
+//  Copyright © 2020 Eshaan Kaul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hey Eshaan!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(
+        )
+    }
+}
